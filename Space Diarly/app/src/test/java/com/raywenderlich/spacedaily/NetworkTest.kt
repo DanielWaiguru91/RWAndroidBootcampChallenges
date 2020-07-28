48d8db3d313f57aa0d5ebf60ec1e878f09eeeeee
Daniel Waiguru <danielirungu91@gmail.com>
2020-07-28 12:08:51 +0300

    Testing Retrofit Instance creation

---
+++ Space Diarly/app/src/test/java/com/raywenderlich/spacedaily/NetworkTest.kt
@@ -0,0 +1,38 @@
+package com.raywenderlich.spacedaily
+
+import com.raywenderlich.spacedaily.di.networkModule
+import com.raywenderlich.spacedaily.network.NASAAPIInterface
+import com.squareup.moshi.Moshi
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.qualifier.named
+import org.koin.test.KoinTest
+import org.koin.test.get
+import org.koin.test.inject
+import retrofit2.Retrofit
+
+class NetworkTest: KoinTest {
+    private val retrofit: Retrofit by inject()
+    val api: NASAAPIInterface by inject()
+    val moshi: Moshi by inject()
+    private val baseUrl : String by lazy { get(named("BASE_URL")) as String }
+    @Before
+    fun setup(){
+        startKoin {
+            modules(listOf(networkModule))
+        }
+    }
+    @After
+    fun tearDown(){
+        stopKoin()
+    }
+    @Test
+    fun `Retrofit Instance Created and BASE_URL is not null` (){
+        assertNotNull(retrofit)
+        assertNotNull(baseUrl)
+    }
+}
