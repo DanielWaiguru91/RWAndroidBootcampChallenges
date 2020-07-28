b9e486b4a87cb6ef2b155c905687bd9f72978ce0
Daniel Waiguru <danielirungu91@gmail.com>
2020-07-28 11:27:10 +0300

    Using Koin to inject network depeendencies

---
+++ Space Diarly/app/src/main/java/com/raywenderlich/spacedaily/DailyPhotoView.kt
@@ -0,0 +1,31 @@
+package com.raywenderlich.spacedaily
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import com.raywenderlich.spacedaily.network.PhotoResponse
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+
+class DailyPhotoView(private val mainView: ViewGroup): MainView, KoinComponent, LayoutContainer {
+    private val activityRetriever: ActivityRetriever by inject()
+    override val containerView: View?
+        get() = mainView
+    init {
+        val viewModel = ViewModelProvider(activityRetriever.getActivity() as FragmentActivity)
+            .get(MainViewModel::class.java)
+        viewModel.view = this
+        viewModel.getDailyPhoto()
+    }
+
+    override fun setDailyPhoto(dailyPhoto: PhotoResponse) {
+        GlideApp.with(activityRetriever.context)
+            .load(dailyPhoto.url)
+            .into(nasaImage)
+        date.text = dailyPhoto.date
+        explanation.text = dailyPhoto.explanation
+    }
+}
--- Space Diarly/app/src/main/java/com/raywenderlich/spacedaily/SpaceApp.kt
+++ Space Diarly/app/src/main/java/com/raywenderlich/spacedaily/SpaceApp.kt
@@ -22,10 +22,22 @@
 package com.raywenderlich.spacedaily
 
 import android.app.Application
+import com.raywenderlich.spacedaily.di.appModule
+import com.raywenderlich.spacedaily.di.networkModule
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.logger.AndroidLogger
+import org.koin.core.context.startKoin
 
 class SpaceApp : Application() {
-
+  val defaultCurrentActivityListener: DefaultCurrentActivityListener by inject()
   override fun onCreate() {
     super.onCreate()
+    startKoin {
+      AndroidLogger()
+      androidContext(this@SpaceApp)
+      modules(listOf(appModule, networkModule))
+    }
+    registerActivityLifecycleCallbacks(defaultCurrentActivityListener)
   }
 }
